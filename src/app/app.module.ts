64f0a4464a4b3c6df03152e5b881d33bda51e5dd
Kartik Kapoor <kartikkapoor13@gmail.com>
2019-06-21 16:19:49 +0800

    Adding Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { IndexComponent } from './home/index/index.component';
+import { AllnotesComponent } from './home/allnotes/allnotes.component';
+import { AddnoteComponent } from './home/addnote/addnote.component';
+import { SinglenoteComponent } from './home/allnotes/singlenote/singlenote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    HomeComponent,
+    IndexComponent,
+    AllnotesComponent,
+    AddnoteComponent,
+    SinglenoteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/allnotes/singlenote/singlenote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-singlenote',
+  templateUrl: './singlenote.component.html',
+  styleUrls: ['./singlenote.component.css']
+})
+export class SinglenoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
